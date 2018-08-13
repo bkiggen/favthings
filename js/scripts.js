b4fd98b9acab9471741887fdda94c748ca592b2f
Ben Kiggen and Dan Mace <ben@>
2018-08-13 09:32:37 -0700

    add top three answers to ul

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,15 @@ $(document).ready(function(){
   $("form#fav-form").submit(function(event){
     event.preventDefault();
     array.push(" " + $("input#fav-input").val());
-    $("#output").text(array);
-    console.log(typeof array);
+    $("#output p").text(array);
+    var newArray = array.slice();
+
+    if (array.length === 3){
+      $("#output ul").append("<li>" + array[0] + "</li>");
+      $("#output ul").append("<li>" + array[1] + "</li>");
+      $("#output ul").append("<li>" + array[2] + "</li>");
+    } else {
+
+    };
   });
 });
